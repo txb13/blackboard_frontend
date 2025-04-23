8e1b82b730d1200a64e5ead40e16b527e38984e0
Felix Siegfried Riedel <f.riedel.business@gmail.com>
2025-04-23 14:31:14 +0200

    add NoteService

---
+++ src/services/NoteService.ts
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+interface Note {
+    title: string;
+    content: string;
+    author: string;
+    id: number;
+    color: string | null;
+    creationDate: string | null;
+    terminationDate: string | null;
+    xPosition: number;
+    yPosition: number;
+    width: number;
+    height: number;
+}
+
+
+export default class NoteService {
+    getNotes(): Promise<Note[]> {
+        return axios
+            .get('http://localhost:8080/notes')
+            .then((response) => {
+                const notes: Note[] = response.data;
+                console.log("GET notes:", notes);
+                return notes;
+            });
+    }
+}
