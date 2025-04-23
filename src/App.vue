7b1617393e2c77f192769cc1536a4a77b263fc17
Felix Siegfried Riedel <f.riedel.business@gmail.com>
2025-04-23 12:42:13 +0200

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -7,83 +7,28 @@ import NoteGUI from './components/NoteGUI.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo_Pinnboard.png" width="150" height="125" />
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img src="@/assets/logo_Pinnboard.png" width="30" height="30" class="d-inline-block align-top" alt="">
+        Pinnboard
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-    <div class="wrapper">
-      <HelloWorld msg="Pinnboard" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <RouterLink to="/" class="nav-link" >Home</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink to="/About" class="nav-link" >About</RouterLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </header>
-
   <RouterView />
-
-
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
