995b8e842a2736d9e83e51ee1d6a765169c6b2f1
Felix Siegfried Riedel <f.riedel.business@gmail.com>
2025-05-13 13:54:33 +0200

    consolidate note and note2, adjust variable names, remove unused drag and drop, remove notes table

--- src/components/NoteGUI.vue
+++ src/components/NoteGUI.vue
@@ -4,95 +4,82 @@
     import CustomNode from '../components/CustomNode.vue'
  **/
 
-import { ref } from 'vue'
-import { VueFlow } from '@vue-flow/core'
+import {ref} from 'vue'
+import {useVueFlow, VueFlow} from '@vue-flow/core'
 import { Background } from '@vue-flow/background'
-import useDragAndDrop from '../useDnD'
-import NoteService from '../services/NoteService.ts'
+import NoteService, {type Note} from '../services/NoteService.ts'
 
-//TODO: Fully implement Note2, remove Note and adjust code
-type Note2 = {
-  title: string;
-  content: string;
-  author: string;
-  id: number;
-  color: string | null;
-  creationDate: string | null;
-  terminationDate: string | null;
-  xPosition: number;
-  yPosition: number;
-  width: number;
-  height: number;
+interface PbNote {
+  id: string
+  type: string
+  position: { x: number; y: number }
+  data: { label: string }
 }
 
-type Note = { id: number; text: string }
-
-const notes = ref<Note[]>([])
-
+const pbNotes = ref<PbNote[]>([])
+const {fitView } = useVueFlow()
 const noteService = new NoteService()
 
-const nodes = ref([
-  {
-    id: '1',
-    type: 'input',
-    position: { x: 250, y: 5 },
-    data: { label: 'Node 1' },
-  }
-])
-
-noteService.getNotes().then((notes2: Note2[]) => {
-  let id = 0;
-  for (const note of notes2) {
-    nodes.value.push({
-      id: String(id),
+noteService.getNotes().then((notes: Note[]) => {
+  for (const note of notes) {
+    pbNotes.value.push({
+      id: String(note.id),
       type: 'input',
       position: { x: note.xPosition, y: note.yPosition },
       data: { label: note.title },
     })
-    id++
   }
-  console.log(nodes.value)
 })
 
+function zoomToNote(target: string) {
+  fitView({
+    nodes: [target],
+    duration: 1000,
+  })
+}
 
-const nameField = ref('')
-const nextId    = ref(1)
+const titleField = ref('')
+const authorField = ref('')
+const contentField = ref('')
 
-//TODO: refresh site after running addNote so created notes appear automatically after posting to backend
-function addNote() {
-  if (!nameField.value.trim()) return
-  notes.value.push({ id: nextId.value++, text: nameField.value.trim() })
-  noteService.addNote({
-    id: 100,
-    title: "Test Title",
-    content: nameField.value,
-    author: "Me",
+async function addNote() {
+  if (!contentField.value.trim()) return
+  await refresh()
+  await noteService.addNote({
+    id: 0,
+    title: titleField.value.trim(),
+    content: contentField.value.trim(),
+    author: authorField.value.trim(),
     color: null,
     creationDate: null,
     terminationDate: null,
-    xPosition: 0,
-    yPosition: 0,
+    xPosition: Math.floor(Math.random() * 1000),
+    yPosition: Math.floor(Math.random() * 400),
     width: 100,
     height: 100
   });
-  noteService.getNotes()
-  nameField.value = ''
+  await refresh()
+  setTimeout(() => {
+    zoomToNote(String(pbNotes.value.length - 1))
+  }, 50)
+  // contentField.value = ''
 }
 
 /* TODO: Notes need to to be removed by clicking an "X", they should also be removed from backend
-     currently they are only removed from the drag and drop list but remain on the board
  */
-function removeNote(id: number) {
-  notes.value = notes.value.filter(n => n.id !== id)
+
+async function refresh() {
+  const notes = await noteService.getNotes()
+  pbNotes.value = notes.map((note, index) => ({
+    id: String(index),
+    type: 'input',
+    position: {x: note.xPosition, y: note.yPosition},
+    data: {label: note.title},
+  }))
+  await fitView({duration: 500})
 }
 
-const {
-  onDragStart,
-  onDragOver,
-  onDragLeave,
-  onDrop,
-  isDragOver,
-} = useDragAndDrop()
+refresh()
 
 </script>
 
@@ -106,47 +93,30 @@ const {
     <form @submit.prevent="addNote">
       <div class="form-row">
         <div class="form-group col-md-6">
-          <input class="form-control" placeholder="Titel">
+          <input class="form-control" v-model="titleField" placeholder="Titel">
         </div>
         <div class="form-group col-md-6">
-          <input class="form-control" placeholder="Autor">
+          <input class="form-control" v-model="authorField" placeholder="Autor">
         </div>
       </div>
       <div class="form-group">
       <textarea class="form-control"
-                v-model="nameField"
+                v-model="contentField"
                 placeholder="Schreibe deine Notiz hier …"
                 rows="3">
       </textarea>
       </div>
       <button type="submit" class="btn btn-primary">Notiz hinzufügen</button>
     </form>
-<!-- TODO: this note list needs to be removed, instead notes should appear on the pinboard and be removed there-->
-    <table class="nodes">
-      <tbody>
-      <tr v-if="!notes.length">
-        <td colspan="2">Noch keine Notizen!</td>
-      </tr>
-
-      <tr v-for="note in notes" :key="note.id">
-        <td><button @click="removeNote(note.id)">X</button></td>
-        <td>
-          <div class="vue-flow__node-input" :draggable="true"  @dragstart="onDragStart($event, { type: 'input', label: note.text })"  >{{ note.text }}</div>
-
-        </td>
-      </tr>
-      </tbody>
-    </table>
+  <div v-if=pbNotes>
+    Anzahl Notizen: {{pbNotes.length-1}}
   </div>
-
+  </div>
 <div class="container-fluid">
   <div class="canvas">
     <VueFlow
         class="board"
-        @dragover.prevent="onDragOver"
-        @dragleave="onDragLeave"
-        @drop="onDrop"
-        :nodes="nodes"
+        :nodes="pbNotes"
         :pan-on-drag="false"
         :pan-on-scroll="false"
         :nodes-draggable="true"
@@ -161,5 +131,4 @@ const {
     </VueFlow>
   </div>
 </div>
-
 </template>
