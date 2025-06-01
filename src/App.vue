e03507bb2b7f9134e60e190f3b9d039db2ab95a3
Felix Siegfried Riedel <f.riedel.business@gmail.com>
2025-06-02 00:07:45 +0200

    align logo and title

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,9 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">
-        <img src="@/assets/logo_Pinnboard.png" width="30" height="30" class="d-inline-block align-top" alt="">
-        Pinnboard
+      <a class="navbar-brand d-flex align-items-center ms-3" href="#">
+        <img src="@/assets/logo_Pinnboard.png" width="30" height="30" class="me-2" alt="">
+        <span>Pinnboard</span>
       </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
